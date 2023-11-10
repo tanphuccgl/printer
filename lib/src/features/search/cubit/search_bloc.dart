10dade5aaae78d788ba8b991f79130f743d09fe9
Levi <phamnguyentanphucgl@gmail.com>
2023-11-11 01:08:08 +0700

    remove-product

--- lib/src/features/search/cubit/search_bloc.dart
+++ lib/src/features/search/cubit/search_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:printer/src/network/domain.dart';
@@ -59,6 +60,34 @@ class SearchBloc extends Cubit<SearchState> {
     XToast.show("Tính năng đang phát triển");
   }
 
+  Future<void> showDeleteConfirmationDialog(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Xác nhận xóa?'),
+          content: const Text('Bạn có chắc chắn muốn xóa sản phẩm này?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Hủy'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+
+                deleteProduct();
+              },
+              child: const Text('Xác nhận'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   void deleteProduct() async {
     if (state.productId.isEmpty) {
       XToast.error("Vui lòng nhập mã sản phẩm");
@@ -70,7 +99,7 @@ class SearchBloc extends Cubit<SearchState> {
     final result = await domain.product.deleteProduct(state.productId);
     if (result.isSuccess) {
       await getAllProduct();
-      XToast.error("Xóa sản phẩm thành công");
+      XToast.success("Xóa sản phẩm thành công");
       emit(SearchState.ds());
       XToast.hideLoading();
       return;
